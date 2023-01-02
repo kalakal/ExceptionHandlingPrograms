53b0d0cb578233b7ed3b1803ff0e7554f9010b63
kalakal <sayedfaizanqadri@gmail.com>
2023-01-02 23:25:41 +0530

    [faizan],voting personException added

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-class BalanceException(Exception):
-    pass
+# exception class
+class VotingPerson(Exception):
+    #   Raised when the input age is less than 18
+    def _inti_(self, name, age):
+        self.name = name
+        self.age = age
+
+    # try taking details of the users,
+    # if the age is inappropriate, throw an exception
 
 
-def checkbalance():
-    money = 10000
-    withdraw = int(input("Enter The Amount"))
-    try:
-        balance = money - withdraw
-        if(balance<=2000):
-            raise BalanceException("insufficient balance")
-        print(balance)
-    except BalanceException as be:
-        print(be)
-
-
-checkbalance()
+try:
+    name = input("Enter name : ")
+    age = int(input("Enter age : "))
+    if age < 18:
+        raise VotingPerson
+    vote1 = VotingPerson(name, age)
+    print("You are eligible for voting")
+except VotingPerson:
+    print("You not eligible for voting")
